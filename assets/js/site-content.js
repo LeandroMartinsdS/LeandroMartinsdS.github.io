16ca431bff0fc5c7520406b3dde738461edce52e
LeandroMartinsdS <leandro.martins.ctrleng@gmail.com>
2026-05-20 08:33:39 +0000

    Reorganise publications

--- assets/js/site-content.js
+++ assets/js/site-content.js
@@ -32,15 +32,15 @@ export const siteContent = Object.freeze({
   ],
   publications: [
     {
-      title: "The Mirror Systems Benches Kinematics Development for Sirius/LNLS",
-      meta: "JACoW ICALEPCS2021 - 2022",
-      href: "https://inspirehep.net/literature/2076524",
-      cta: "Read",
-    },
-    {
       title: "The Control System of the Four-Bounce Crystal Monochromators for SIRIUS/LNLS Beamlines",
       meta: "JACoW ICALEPCS2021 - 2022",
       href: "https://inspirehep.net/literature/2076537",
+      cta: "Read",
+    },
+    {
+      title: "The Mirror Systems Benches Kinematics Development for Sirius/LNLS",
+      meta: "JACoW ICALEPCS2021 - 2022",
+      href: "https://inspirehep.net/literature/2076524",
       cta: "Read",
     },
     {
